3b63fd4c668467083cd06cee144fcdbcde98da26
Aimee <90500304+aimeeest@users.noreply.github.com>
2023-01-10 16:44:44 +0800

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      //  \App\Models\User::factory(10)->create();
+
+       $user= User::factory()->create([
+             'name' => 'Test User',
+             'email' => 'test@example.com',
+             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+         ]);
+
+        $role = Role::create(['name' => 'super_admin']);
+        $user->assignRole($role);
+
+    }
+}
